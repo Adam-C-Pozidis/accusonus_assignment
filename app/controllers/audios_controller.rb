cf421e2f677ca9a77e0aff13f2447dff58442aa1
Adam-C-Pozidis <adchrpoz@gmail.com>
2021-05-29 12:08:39 +0200

    set up actions on controller and model with cloudinary

--- app/controllers/audios_controller.rb
+++ app/controllers/audios_controller.rb
@@ -1,4 +1,36 @@
 class AudiosController < ApplicationController
+  before_action :set_audio, only: [:show, :edit, :update, :destroy]
   def index
+    @audios = Audio.all
+    @audio = Audio.new
   end
+
+  def show
+  end
+  
+  def new
+    @audio = Audio.new(audio_params)
+  end
+
+  def create
+    @audio = Audio.new(audio_params)
+    if @audio.save
+      redirect_to root_path
+    else
+      render :root
+    end
+  end
+
+  def edit
+  end
+
+
+    private
+    def set_audio
+      @audio = Audio.find(params[:id])
+    end
+
+    def audio_params
+      params.require(:audio).permit(:name, :video)
+    end
 end
--- app/models/audio.rb
+++ app/models/audio.rb
@@ -1,2 +1,3 @@
 class Audio < ApplicationRecord
+    has_one_attached :video
 end
